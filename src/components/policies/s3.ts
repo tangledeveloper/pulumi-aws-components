ea27c33521ed23b709ae617d5807b64e52ed5c43
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2020-06-22 09:51:40 +0200

    feat: introduce readwrite policy

--- src/components/policies/s3.ts
+++ src/components/policies/s3.ts
@@ -1,10 +1,13 @@
 import * as aws from '@pulumi/aws'
 import * as pulumi from '@pulumi/pulumi'
 
-interface S3ReadPolicyArgs {
+interface S3PolicyArgs {
   name: string
   bucketArn: pulumi.Output<aws.ARN>
 }
+
+type S3ReadPolicyArgs = S3PolicyArgs
+type S3ReadWritePolicyArgs = S3PolicyArgs
 
 export class S3ReadPolicy extends pulumi.ComponentResource {
   readonly policy: aws.iam.Policy
@@ -13,7 +16,7 @@ export class S3ReadPolicy extends pulumi.ComponentResource {
     super('aws:components:S3ReadPolicy', args.name, args, opts)
     const { name, bucketArn } = args
     const defaultParentOptions: pulumi.ResourceOptions = { parent: this }
-    const policy = new aws.iam.Policy(
+    this.policy = new aws.iam.Policy(
       name,
       {
         name,
@@ -30,6 +33,39 @@ export class S3ReadPolicy extends pulumi.ComponentResource {
       },
       defaultParentOptions
     )
-    this.policy = policy
+
+    this.registerOutputs({ policy: this.policy })
+  }
+}
+
+export class S3ReadWritePolicy extends pulumi.ComponentResource {
+  readonly policy: aws.iam.Policy
+  constructor(args: S3ReadWritePolicyArgs, opts?: pulumi.ComponentResourceOptions) {
+    super('aws:components:S3ReadWritePolicy', args.name, args, opts)
+    const defaultParentOptions: pulumi.ResourceOptions = { parent: this }
+    this.policy = new aws.iam.Policy(
+      args.name,
+      {
+        policy: pulumi.output(args.bucketArn).apply(arn =>
+          JSON.stringify({
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Action: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
+                Effect: 'Allow',
+                Resource: `${arn}/*`
+              },
+              {
+                Action: ['s3:ListBucket'],
+                Effect: 'Allow',
+                Resource: arn
+              }
+            ]
+          })
+        )
+      },
+      defaultParentOptions
+    )
+    this.registerOutputs({ policy: this.policy })
   }
 }
