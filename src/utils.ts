82088630680c001daf3b3c417177c0b202e39444
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2020-07-11 10:23:35 +0200

    use rolename aspart of the attachment

--- src/utils.ts
+++ src/utils.ts
@@ -9,10 +9,10 @@ export function attachPoliciesToRole(role: aws.iam.Role, policies: aws.iam.Polic
   const rolePolicyAttachments: pulumi.Input<aws.iam.RolePolicyAttachment>[] = []
   for (const policy of policies) {
     rolePolicyAttachments.push(
-      pulumi.output(policy.name).apply(
-        policyName =>
+      pulumi.all([policy.name, role.name]).apply(
+        ([policyName, roleName]) =>
           new aws.iam.RolePolicyAttachment(
-            `${policyName}-role-attachment`,
+            `${policyName}-${roleName}-role-attachment`,
             {
               policyArn: policy.arn,
               role
